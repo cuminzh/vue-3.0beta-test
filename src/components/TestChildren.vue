44d645f4a641938e289f5860fa25896781a961bf
cuminzh <cuminzh@wicrenet.com>
2020-04-26 18:52:50 +0800

    test

---
+++ src/components/TestChildren.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>{{num}}</div>
+  <div>{{nums}}</div>
+  <button @click="add">add</button>
+</template>
+
+<script>
+  import { watch, ref, computed } from 'vue'
+
+  export default {
+    name: 'TestChildren',
+    props: {
+      childCount: Number
+    },
+    setup (props) {
+      const num = ref(props.childCount + 1)
+      let nums = computed(() => num.value + 1)
+      const add = () => {
+        num.value++
+      }
+      watch(num, val => {
+        console.log(val)
+        // nums = 3
+      })
+      return {
+        num,
+        nums,
+        add
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="div-test">{{count}} {{object.foo}}</div>
+  <test-children :child-count="count"/>
+</template>
+
+<script>
+  import {h, ref, reactive } from 'vue';
+  import TestChildren from '../components/TestChildren'
+
+  export default {
+    name: 'Test',
+    components: {TestChildren},
+    setup () {
+      const count = ref(0)
+      const object = reactive({ foo: 'bar' })
+      // return () => h('div', [count.value, object.foo])
+      return {
+        count,
+        object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
